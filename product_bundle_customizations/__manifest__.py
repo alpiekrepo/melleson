158e7f5def1d70caff9e711608067b853ad3feb2
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2020-11-30 12:09:53 +0100

    MEL-46552 product_bundle_customizations: added customizations module

---
+++ product_bundle_customizations/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Product Bundle Customizations",
+    "category": '',
+    "description": """
+        Customizations on module product bundle pack for Melleson.
+    """,
+    "author": "Alpiek",
+    "website": "https://alpiek.nl",
+    "version": '13.0.1.0',
+    "depends": [
+        'product_bundle_pack',
+     ] ,
+    "data": [
+        'views/product_view.xml',
+    ],
+    "application": False,
+    "license": u"OPL-1",
+}
---
+++ product_bundle_customizations/models/product.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models, _
+
+
+class ProductProduct(models.Model):
+	_inherit = 'product.template'
+
+	max_packs = fields.Integer(
+		string=_("Maximum amount of packs available"),
+		compute="_compute_max_packs",
+	)
+
+	@api.depends('pack_ids')
+	def _compute_max_packs(self):
+		for r in self:
+			r.max_packs = min([item.product_id.qty_available / item.qty_uom for item in r.pack_ids])
