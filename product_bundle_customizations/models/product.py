7bd546f647bba26589910719b4bb3580b86f714c
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2020-11-30 15:24:37 +0100

    MEL-46552 product_bundle_customizations: added related field

--- product_bundle_customizations/models/product.py
+++ product_bundle_customizations/models/product.py
@@ -2,14 +2,26 @@ from odoo import api, fields, models, _
 
 
 class ProductProduct(models.Model):
-	_inherit = 'product.template'
+	_inherit = "product.template"
 
 	max_packs = fields.Integer(
 		string=_("Maximum amount of packs available"),
 		compute="_compute_max_packs",
 	)
 
-	@api.depends('pack_ids')
+	test_list = fields.Many2one(
+		comodel_name="product.product",
+		default=None,
+		compute="_compute_test_list",
+		store=True,
+	)
+
+	def _compute_test_list(self):
+		for r in self:
+			if r.is_pack and r.pack_ids:
+				r.test_list = [item.product_id.id for item in r.pack_ids]
+
+	@api.depends("pack_ids")
 	def _compute_max_packs(self):
 		for r in self:
 			if r.is_pack:
