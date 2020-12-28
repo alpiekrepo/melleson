196813bdebc1fa4dd6ea8c325e01d423a0de70ea
Konrad Szymański <ko.szymanski@gmail.com>
2020-12-28 22:20:21 +0100

    MEL-46512 logistics_info

---
+++ logistics_info/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Logistics Info',
+    'version': '13.0.1.0.0',
+    'description': """
+    Logistic tab in product and partner
+    """,
+    'author': "Alpiek",
+    'website': "https://alpiek.nl/",
+    'depends': [
+        'product',
+        'base',
+    ],
+    'data': [
+        'views/product_view.xml',
+        'views/res_partner_view.xml',
+    ],
+    'application': False,
+    'license': u'OPL-1',
+}
